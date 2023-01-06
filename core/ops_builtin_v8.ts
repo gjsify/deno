10903c18a3d5cec21fa817d182ea4b5ecb0f6f9c
Pascal Garber <pascal@artandcode.studio>
2023-01-06 13:59:54 +0100

    feat(deno-runtime): Add implementation for op_set_promise_reject_callback, op_run_microtasks and more

--- core/ops_builtin_v8.ts
+++ core/ops_builtin_v8.ts
@@ -1,4 +1,9 @@
 import GLib from "@gjsify/types/GLib-2.0";
+import { byteArray } from "@gjsify/types/Gjs";
+import { logSignals } from "@gjsify/utils";
+
+let has_tick_scheduled = false;
+const pending_promise_exceptions: {promise: Promise<any>, reason: any}[] = [];
 
 export const op_ref_op = (promiseId: number) => {
   console.warn("Not implemented: ops.op_ref_op");
@@ -19,19 +24,33 @@ export const op_set_next_tick_callback = (fn: () => void): void => {
 }
 
 export const op_set_promise_reject_callback = (fn: Deno.core.PromiseRejectCallback): void => {
-  console.warn("Not implemented: ops.op_set_promise_reject_callback");
+  logSignals.connect("unhandledRejection", (self, data, promiseData) => {
+
+    /**
+     * TODO: Not sure how we should implement this:
+     * 0: store pending promise exception
+     * 1: remove pending promise exception
+     */
+    const type: 0 | 1 = 1;
+
+    fn(type, promiseData.promise, promiseData.reason);
+  });
 }
 
 export const op_run_microtasks = () => {
-  console.warn("Not implemented: ops.op_run_microtasks");
+  // In Deno this method runs the microtask which a in the microtask queue added with `op_queue_microtask`,
+  // but we let GLib do this. If this should lead to problems, we can still adjust the implementation.
+
+  // But we can force the execution of idle callbacks
+  GLib.main_context_default().iteration(false);
 }
 
 export const op_has_tick_scheduled = () => {
-  console.warn("Not implemented: ops.op_has_tick_scheduled");
+  return has_tick_scheduled;
 }
 
 export const op_set_has_tick_scheduled = (bool: boolean) => {
-  console.warn("Not implemented: ops.op_set_has_tick_scheduled");
+  has_tick_scheduled = bool;
 }
 
 export const op_eval_context = (source: string, specifier: string) => {
@@ -56,13 +75,11 @@ export const op_create_host_object = (): object => {
 }
 
 export const op_encode = (text: string): Uint8Array => {
-  console.warn("Not implemented: ops.op_encode");
-  return new Uint8Array();
+  return byteArray.fromString(text);
 }
 
 export const op_decode = (buffer: Uint8Array): string => {
-  console.warn("Not implemented: ops.op_decode");
-  return "";
+  return byteArray.toString(buffer);
 }
 
 export const op_serialize = (value, options = {}, errorCallback?): any => {
@@ -140,17 +157,20 @@ export const op_event_loop_has_more_work = (): void => {
   console.warn("Not implemented: ops.op_event_loop_has_more_work");
 }
 
-export const op_store_pending_promise_exception = (promise, reason) => {
-  console.warn("Not implemented: ops.op_store_pending_promise_exception");
+export const op_store_pending_promise_exception = (promise: Promise<any>, reason: any) => {
+  pending_promise_exceptions.push({
+    promise,
+    reason
+  });
 }
 
-export const op_remove_pending_promise_exception = (promise) => {
-  console.warn("Not implemented: ops.op_remove_pending_promise_exception");
+export const op_remove_pending_promise_exception = (promise: Promise<any>) => {
+  const index = pending_promise_exceptions.findIndex((el) => el.promise === promise);
+  pending_promise_exceptions.splice(index, 1);
 }
 
-export const op_has_pending_promise_exception = (promise) => {
-  console.warn("Not implemented: ops.op_has_pending_promise_exception");
-  return false;
+export const op_has_pending_promise_exception = (promise: Promise<any>) => {
+  return !!pending_promise_exceptions.find((el) => el.promise === promise);
 }
 
 export const op_arraybuffer_was_detached = (O: ArrayBufferLike): boolean => {
